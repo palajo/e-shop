9c3069c4f084d23cddecaeb199126eb4ee4565b2
Oleh Palazhii <palajoleg@gmail.com>
2021-05-17 23:06:22 +0300

    update

---
+++ server/routers/categorySymbolsRouter.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+
+import axios from 'axios';
+import crypto from 'crypto';
+import httpBuildQuery from 'http-build-query';
+import sortKeys from 'sort-keys';
+
+const categorySymbolsRouter = express.Router();
+
+    // making request to api.tme.eu
+    const apiData = []
+
+    const token = "a1dbf8d9dcbe22b39f85fc29ee76824738163c55348ae"
+    const secret = "9afc3d89fa3ace87c52f"
+
+    const domain = "https://api.tme.eu/"
+    const path = "Products/GetSymbols"
+    const format = ".json"
+    const url = domain + path + format;
+
+    const params = {
+        Country: 'UA',
+        Language: 'uk',
+        CategoryId: '100181',
+        Token: token
+    }
+
+    const sortedParams = sortKeys(params, { deep: true })
+    const queryParams = httpBuildQuery(sortedParams)
+
+    const signatureRequest = 'POST&' + encodeURIComponent(url) + "&" + encodeURIComponent(queryParams)
+    const signature = crypto.createHmac('sha1', secret).update(signatureRequest, 'utf8').digest('base64')
+
+    params['ApiSignature'] = signature
+
+    const axiosSettings = {
+        url: url,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: params
+    }
+
+    axios.request(axiosSettings)
+        .then((res) => { 
+            apiData.push(res.data)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
+
+categorySymbolsRouter.get( '/', expressAsyncHandler(async (req, res) => {
+    res.send(apiData);
+}));
+
+export default categorySymbolsRouter;
---
+++ server/server.js
@@ -0,0 +1,35 @@
+import http from 'http'
+import express from 'express'
+import cors from 'cors'
+
+import productRouter from './routers/productRouter.js';
+import categoriesRouter from './routers/categoriesRouter.js';
+import categoryProductsRouter from './routers/categoryProductsRouter.js';
+import categorySymbolsRouter from './routers/categorySymbolsRouter.js';
+
+process.on('SIGINT', () => { 
+    process.exit();
+});
+
+process.on('exit', () => { 
+    process.exit();
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/product/', productRouter);
+app.use('/api/catalog/', categoriesRouter);
+app.use('/api/categorySymbols/', categorySymbolsRouter);
+app.use('/api/categoryProducts/', categoryProductsRouter);
+
+const httpServer = http.Server(app);
+
+const port = process.env.PORT || 5000;
+
+httpServer.listen(port, () => {
+    console.log('Server is ran at http://localhost:5000/')
+})
